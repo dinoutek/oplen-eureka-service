9f399e81641adfb283101359c38beaf91655ec2a
nordine <dinoutek@gmail.com>
2022-10-25 10:53:55 +0200

    Initial commit

---
+++ src/test/java/com/example/opleneurekaservice/OplenEurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.opleneurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OplenEurekaServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
